0b4e64cefd0c94cb3a8e636107e515f0cdcc92c0
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-12-04 11:37:15 +0200

    Components generated from Article and more

---
+++ components/ArticleRender.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="relative py-16 bg-white overflow-hidden">
+    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+      <div
+        class="relative h-full text-lg max-w-prose mx-auto"
+        aria-hidden="true"
+      >
+        <svg
+          class="absolute top-12 left-full transform translate-x-32"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+        >
+          <defs>
+            <pattern
+              id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="384"
+            fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+          />
+        </svg>
+        <svg
+          class="
+            absolute
+            top-1/2
+            right-full
+            transform
+            -translate-y-1/2 -translate-x-32
+          "
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+        >
+          <defs>
+            <pattern
+              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="384"
+            fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+          />
+        </svg>
+        <svg
+          class="absolute bottom-12 left-full transform translate-x-32"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+        >
+          <defs>
+            <pattern
+              id="d3eb07ae-5182-43e6-857d-35c643af9034"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="384"
+            fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+          />
+        </svg>
+      </div>
+    </div>
+    <div class="relative px-4 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1>
+          <span
+            class="
+              mt-2
+              block
+              text-3xl text-center
+              leading-8
+              font-extrabold
+              tracking-tight
+              text-gray-900
+              sm:text-4xl
+            "
+            >{{ article.title }}</span
+          >
+        </h1>
+        <img class="my-10" :src="article.image" />
+      </div>
+      <div class="prose prose-indigo prose-lg text-gray-500 mx-auto">
+        <div class="flex flex-row-reverse">
+          <div class="flex items-center">
+            <div>
+              <img
+                class="inline-block h-9 w-9 rounded-full mr-2"
+                style="margin: 0"
+                :src="article.author_avatar"
+                alt=""
+              />
+            </div>
+            <div class="ml-3">
+              <p
+                class="
+                  text-sm
+                  font-medium
+                  text-gray-700
+                  group-hover:text-gray-900
+                "
+              >
+                {{ article.author }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <nuxt-content :document="article" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
---
+++ components/MoreArticles.vue
@@ -0,0 +1,92 @@
+<template>
+  <div
+    class="
+      relative
+      bg-gray-50
+      pt-16
+      pb-20
+      px-4
+      sm:px-6
+      lg:pt-24 lg:pb-28 lg:px-8
+    "
+  >
+    <div class="absolute inset-0">
+      <div class="bg-white h-1/3 sm:h-2/3" />
+    </div>
+    <div class="relative max-w-7xl mx-auto">
+      <div class="text-center">
+        <h2
+          class="
+            text-3xl
+            tracking-tight
+            font-extrabold
+            text-gray-900
+            sm:text-4xl
+          "
+        >
+          More articles
+        </h2>
+      </div>
+      <div
+        class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"
+      >
+        <div
+          v-for="(article, key) in articles"
+          :key="key"
+          class="flex flex-col rounded-lg shadow-lg overflow-hidden"
+        >
+          <div class="flex-shrink-0">
+            <img class="h-48 w-full object-cover" :src="article.image" alt="" />
+          </div>
+          <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+            <div class="flex-1">
+              <a href="#" class="block mt-2">
+                <p class="text-xl font-semibold text-gray-900">
+                  {{ article.title }}
+                </p>
+                <p class="mt-3 text-base text-gray-500">
+                  {{ article.description }}
+                </p>
+              </a>
+            </div>
+            <div class="mt-6 flex items-center">
+              <div class="flex-shrink-0">
+                <a href="#">
+                  <span class="sr-only">{{ article.author }}</span>
+                  <img
+                    class="h-10 w-10 rounded-full"
+                    :src="article.author_avatar"
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div class="ml-3">
+                <p class="text-sm font-medium text-gray-900">
+                  <a href="#" class="hover:underline">
+                    {{ article.author }}
+                  </a>
+                </p>
+                <div class="flex space-x-1 text-sm text-gray-500">
+                  <time :datetime="article.createdAt">
+                    {{ article.createdAt }}
+                  </time>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    articles: {
+      required: true,
+      type: Array,
+    },
+  },
+};
+</script>
