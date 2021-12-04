517c53839918a2850deefe4e0456cea51f12d0a8
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-12-04 11:49:49 +0200

    Article linking added

--- components/MoreArticles.vue
+++ components/MoreArticles.vue
@@ -40,31 +40,31 @@
           </div>
           <div class="flex-1 bg-white p-6 flex flex-col justify-between">
             <div class="flex-1">
-              <a href="#" class="block mt-2">
+              <nuxt-link :to="article.path" class="block mt-2">
                 <p class="text-xl font-semibold text-gray-900">
                   {{ article.title }}
                 </p>
                 <p class="mt-3 text-base text-gray-500">
                   {{ article.description }}
                 </p>
-              </a>
+              </nuxt-link>
             </div>
             <div class="mt-6 flex items-center">
               <div class="flex-shrink-0">
-                <a href="#">
+                <nuxt-link :to="article.path">
                   <span class="sr-only">{{ article.author }}</span>
                   <img
                     class="h-10 w-10 rounded-full"
                     :src="article.author_avatar"
                     alt=""
                   />
-                </a>
+                </nuxt-link>
               </div>
               <div class="ml-3">
                 <p class="text-sm font-medium text-gray-900">
-                  <a href="#" class="hover:underline">
+                  <nuxt-link :to="article.path" class="hover:underline">
                     {{ article.author }}
-                  </a>
+                  </nuxt-link>
                 </p>
                 <div class="flex space-x-1 text-sm text-gray-500">
                   <time :datetime="article.createdAt">
